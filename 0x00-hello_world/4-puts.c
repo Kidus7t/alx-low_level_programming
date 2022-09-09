5f550c60af1f0e904028ad14b0959199c2a9d632
Kidus <kidustesfamariam342@gmail.com>
2022-09-09 08:44:24 -0700

    correction on q-4

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,6 +1,12 @@
 #include <stdio.h>
-
-int main(void){
+/**
+ * main- printing using thw puts function
+ *
+ * it should print "Programming is like building a multilingual puzzle
+ * it should return 0
+ */
+int main(void)
+{
 puts("\"Programming is like building a multilingual puzzle");
 return 0;
 }
