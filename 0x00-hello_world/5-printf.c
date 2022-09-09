385ca156e7776eb83e5f8673a3944856122e808e
Kidus <kidustesfamariam342@gmail.com>
2022-09-09 09:03:50 -0700

    correction q-5

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,5 +1,14 @@
 #include <stdio.h>
-int main(void) {
+
+/**
+ * main -printing a string using the printf function
+ *
+ * Description- using the main function
+ * it should print with proper grammar, but the outcome is a piece of art
+ * Return: 0
+ */
+int main(void) 
+{
 printf("with proper grammar, but the outcome is a piece of art,\n");
-	return 0;
+return (0);
 }
