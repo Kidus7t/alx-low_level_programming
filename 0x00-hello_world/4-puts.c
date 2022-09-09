2a7fda008213f382c2ca25b7b91c826feafc8dd2
Kidus <kidustesfamariam342@gmail.com>
2022-09-09 08:51:53 -0700

    q-4 correct

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,10 +1,11 @@
 #include <stdio.h>
+
 /**
  * main- printing using the puts function
  *
  * Description- using the main function
  * it should print "Programming is like building a multilingual puzzle
- * it should return 0
+ * Return: 0
  */
 int main(void)
 {
