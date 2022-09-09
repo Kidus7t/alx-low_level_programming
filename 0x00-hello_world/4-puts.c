049bbad9ac8016af0640ac57c12cea0ac88ba5ff
Kidus <kidustesfamariam342@gmail.com>
2022-09-09 08:48:42 -0700

    q-4

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,12 +1,13 @@
 #include <stdio.h>
 /**
- * main- printing using thw puts function
+ * main- printing using the puts function
  *
+ * Description- using the main function
  * it should print "Programming is like building a multilingual puzzle
  * it should return 0
  */
 int main(void)
 {
 puts("\"Programming is like building a multilingual puzzle");
-return 0;
+return (0);
 }
