75d7cbde41523d6cb195bedc70e73c2a3abade29
Kidus <kidustesfamariam342@gmail.com>
2022-09-09 09:18:21 -0700

    q-6 correction

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -14,8 +14,8 @@ long long ll;
 float f;
 printf("Size of a char: %ld byte(s)\n", sizeof(c));
 printf("Size of an int: %ld byte(s)\n", sizeof(i));
-printf("Size of an long int: %ld byte(s)\n", sizeof(l));
-printf("Size of an long long int: %ld byte(s)\n", sizeof(ll));
-printf("Size of an float: %ld byte(s)\n", sizeof(f));
+printf("Size of a long int: %ld byte(s)\n", sizeof(l));
+printf("Size of a long long int: %ld byte(s)\n", sizeof(ll));
+printf("Size of a float: %ld byte(s)\n", sizeof(f));
 return (0);
 }
