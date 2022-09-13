bf257eda9a8f9e6e2d92e401d7fe3f6a8827d2cf
Kidus <kidustesfamariam342@gmail.com>
2022-09-13 15:07:02 -0700

    1-5

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+ * print_alphabet_x10-print 10 times the alphabet
+ * Return: 0
+ */
+void print_alphabet(void)
+{       char c;
+	int num;
+for (num = 1 ; num <= 10 ; num++)
+{
+for (c = 'a' ; c <= 'z' ; c++)
+{
+_putchar(c);
+}
+_putchar('\n');
+}
+}
---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+ * _islower -checking for lowercases
+ * @c: will be used
+ * Return: 0
+ */
+int _islower(int c)
+{
+char cl;
+if (cl >= 'a' && cl <= 'z')
+{
+	return (1);
+}
+else
+return (0);
+}
