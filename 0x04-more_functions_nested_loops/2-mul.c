d961786f6edfe067cbb8af9e540dce30d33cafba
Kidus <kidustesfamariam342@gmail.com>
2022-09-15 11:23:35 -0700

    0-3

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * mul- multiplying integers
+ * @a: tested character
+ * @b: tested character
+ * Return: always 0
+ */
+
+int mul(int a, int b)
+{
+return (a * b);
+}
+
