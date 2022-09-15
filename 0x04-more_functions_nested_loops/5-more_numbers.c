2a7f8ff90f78d3531dc29e9201e2ba8ca5fa2b58
Kidus <kidustesfamariam342@gmail.com>
2022-09-15 12:42:27 -0700

    q-5 3

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -15,6 +15,16 @@ for (s = 0; s <= 9; s++)
 {
 for (c = 0; c < 15; c++)
 {
+if (c == 10)
+{
+_putchar(c / 10 + '0');
+}
+else if (c > 10)
+{
+_putchar(1);
+_putchar(c % 10);
+}
+else
 _putchar(c);
 }
 _putchar('\n');
